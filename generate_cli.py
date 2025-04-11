869e5b861f08f93ba374c5ab32f115d387a91a3b
viewsdevelop <viewsdevelop@gmail.com>
2025-04-11 11:12:05 -0400

    initial commit

---
+++ generate_cli.py
@@ -0,0 +1,8 @@
+"""
+Entry point for the CLI tool.
+"""
+
+# TODO: Import the main function from lib.cli_tool
+
+# TODO: Create an if __name__ == "__main__" block
+# Inside it, call main()
---
+++ lib/cli_tool.py
@@ -0,0 +1,18 @@
+# NOTE: This starter code is intentionally incomplete so all tests fail.
+# Students will need to define the parser, subcommands, and handlers.
+
+# TODO: Import the argparse module
+
+# TODO: Define the add_task function
+# This function should accept 'args' and print: ✅ Task added: <description>
+
+# TODO: Define the list_tasks function
+# This function should accept 'args' and print: 📋 Listing all tasks...
+
+# TODO: Define the main() function
+# Inside main():
+# - Create an ArgumentParser with a helpful description
+# - Add subparsers for "add" and "list" commands
+# - For "add", require a "description" argument and set its handler
+# - For "list", just set the handler to list_tasks
+# - Parse the arguments and call the appropriate handler (if exists)
