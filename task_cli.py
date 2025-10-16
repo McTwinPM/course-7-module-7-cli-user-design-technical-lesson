0f085cb91f02296ece5759a9d5d5fe9978d1b09a
Michael McCormack <mcctwin@gmail.com>
2025-10-16 16:13:25 -0500

    Add structured CLI for task manager

--- task_cli.py
+++ task_cli.py
@@ -1,8 +1,5 @@
-"""
-Entry point for the CLI tool.
-"""
+from lib.cli_tool import main
 
-# TODO: Import the main function from lib.cli_tool
 
-# TODO: Create an if __name__ == "__main__" block
-# Inside it, call main()
+if __name__ == "__main__":
+    main()
